73d4f734fff6f8105da1ab509a9584fb73294199
Randy <randyisbusy@gmail.com>
2019-12-29 21:39:03 -0700

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,9 +8,29 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @recipe = Recipe.new
+    @recipe = Recipe.new 
+    @recipe.ingredients.build(name: nil) 
+    @recipe.ingredients.build(name: nil)
   end
 
-  def create
-  end
+  def create 
+    @recipe = Recipe.create(recipe_params)
+    
+    # @recipe.ingredients_attributes = ingredients_params                         
+    
+    if @recipe 
+       @recipe.save 
+       
+      redirect_to recipe_path(@recipe) 
+    else 
+      render :new
+    end
+  end 
+
+  private 
+def recipe_params 
+  params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
+end
+
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients  
+  accepts_nested_attributes_for :ingredients 
+  def ingredients=(ingredients_params)
+    self.ingredients.build(ingredients_params) 
+    
+  end
+
+  
 end
